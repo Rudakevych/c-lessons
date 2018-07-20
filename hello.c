a6af78466e0ee7217d95c03357f96b562d08642d
yr <yr@playtini.ua>
2018-07-20 16:50:18 +0300

    Initial commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+//
+// Created by yr on 2018-07-19.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ vars.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int main()
+{
+    int num = 100;
+    double pi = 3.1415926536;
+
+    printf("Integer is %d \n", num);
+    printf("Values are ");
+
+}
+
