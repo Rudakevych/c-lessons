540f79880c9436116b9649e443af23dc98787803
yr <yr@playtini.ua>
2018-07-20 18:11:35 +0300

    added vars file

--- vars.c
+++ vars.c
@@ -6,7 +6,13 @@ int main()
     double pi = 3.1415926536;
 
     printf("Integer is %d \n", num);
-//    printf("Values are ");
+    printf("Values are %d and %f \n", num, pi);
+    printf("%%7d displays %7d \n", num);
+    printf("%%07d displays %07d \n", num);
+    printf("Pi is approximateli %1.10f \n", pi);
+    printf("Right-aligned %20.3f rounded pi \n", pi);
+    printf("Left-aligned %-20.3f rounded pi \n", pi);
 
+    return 0;
 }
 
