bdd6a1f5f7751b77e073c347aab6edb0c79fcfe2
Rudakevych <yurij.rudakevych@gmail.com>
2018-07-22 12:15:20 +0300

    added lessons to page 54

--- define.c
+++ define.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+#define LINE "_____________"
+#define TITLE "C programming in easy steps"
+#define AUTHOR "Mike McGrath"
+
+#ifdef _WIN32
+#define SYSTEM "Windows"
+#endif
+
+#ifdef linux
+#define SYSTEM "LINUX"
+#endif
+
+int main () {
+    printf("\n \t %s \n \t %s \n", LINE, TITLE);
+    printf("\t by %s \n \t %s \n", AUTHOR, LINE);
+    printf("\n Operating System: %s \n", SYSTEM);
+
+    // use command in terminal
+    // cpp -dN define
+    // to see all defined constants
+
+    return 0;
+}
