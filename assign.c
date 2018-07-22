634d04a620fdc4ebc1be595b339a0950bd0f6490
Rudakevych <yurij.rudakevych@gmail.com>
2018-07-22 12:56:49 +0300

    added lessons to page 58

---
+++ assign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main () {
+    int a, b;
+
+    printf("Assigned: \n");
+    printf("\tVatiable a = %d \n", a = 8);
+    printf("\tVariable b = %d \n", b = 4);
+
+    printf("Added & assigned: \n");
+    printf("\tVariable a+=b (8+=4) a = %d \n", a += b);
+
+    printf("Subtracted & assigned: \n");
+    printf("\tVariable a*=b (8*=4) a= %d \n", a *= b);
+
+    printf("Divided & assigned: \n");
+    printf("\tVariable a/=b (32/=4) a= %d \n", a /= b);
+
+    printf("Modulated & assigned: \n");
+    printf("\tVariable a%%=b (8%%=4) a= %d \n", a %= b);
+
+    return 0;
+}
